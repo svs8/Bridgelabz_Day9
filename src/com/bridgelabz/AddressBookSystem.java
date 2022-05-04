b5fb3294b8408ea1818994d2662c09768a1d1d52
svs8 <surakshithvittalshetty@gmail.com>
2022-05-04 17:46:18 +0530

    Added:Ability to add new contacts to address book from console

--- src/com/bridgelabz/AddressBookSystem.java
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -5,42 +5,41 @@ import java.util.Scanner;
      String firstname;
      String lastname;
      String city;
-     String state;
-     long pnumber;
+     long phoneNumber;
 
 
-     contacts(String firstname,String lastname,String city,String state,long pnumber)
+    public contacts(String firstname,String lastname,String city,long phoneNumber)
      {
+         System.out.println("The Contact Details are as follows");
          this.firstname=firstname;
          this.lastname=lastname;
          this.city=city;
-         this.state=state;
-         this.pnumber=pnumber;
+         this.phoneNumber=phoneNumber;
 
-         System.out.println(this.firstname);
-         System.out.println(this.lastname);
-         System.out.println(this.city);
-         System.out.println(this.state);
-         System.out.println(this.pnumber);
+         System.out.println("Name of the contact is : "+this.firstname+" "+this.lastname);
+         System.out.println("City is "+this.city);
+         System.out.println("Phone number= "+this.phoneNumber);
 
      }
 
 
+
 }
-
-
 
 public class AddressBookSystem {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to address Book");
-        System.out.println("The details of first contact");
-        contacts c1= new contacts("RAM","A","bangalore","Karnataka",755980522);
-        System.out.println("The details of Second contact");
-        contacts c2= new contacts("SHAM","B","bangalore","Karnataka",264515123);
-        System.out.println("The details of third contact");
-        contacts c3= new contacts("RAJU","C","bangalore","Karnataka",983726152);
+        Scanner s1=new Scanner(System.in);
+        System.out.println("please enter the details of first contact(firstname,lastname,city,state and phone number)");
+        contacts c1= new contacts(s1.nextLine(),s1.nextLine(),s1.nextLine(),s1.nextLong());
+        Scanner s2=new Scanner(System.in);
+        System.out.println("Please enter the details of second contact(firstname,lastname,city,state and phone number)");
+        contacts c2= new contacts(s2.nextLine(),s2.nextLine(),s2.nextLine(),s2.nextLong());
+        Scanner s3=new Scanner(System.in);
+        System.out.println("Please enter the details of third contact(firstname,lastname,city,state and phone number)");
+        contacts c3= new contacts(s3.nextLine(),s3.nextLine(),s3.nextLine(),s3.nextLong());
 
 
         }
